d4518adc92a248c45b45f8f29a50af9007daf8b6
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-02-13 16:46:14 +0100

    .

--- user/Juules32/mechanics/Banana.gd
+++ user/Juules32/mechanics/Banana.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+
+
+func _ready():
+	get_tree().get_root().get_node("BaseLevel/CanvasLayer/Label").number_of_bananas += 1
+
 func _on_body_entered(body):
 	if body.name == "Player" and self.visible:
 		self.visible = false
--- user/Juules32/mechanics/JumpPad.gd
+++ user/Juules32/mechanics/JumpPad.gd
@@ -2,8 +2,6 @@ extends Area2D
 
 @export var jump_force = -500
 
-
-
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.velocity.y = jump_force
