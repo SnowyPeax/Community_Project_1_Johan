adf8c37bfa9a73695aa0baa8dca33a8f1f87f544
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-03-05 15:21:53 +0100

    more bananers

--- user/Juules32/mechanics/Banana.gd
+++ user/Juules32/mechanics/Banana.gd
@@ -3,10 +3,10 @@ extends Area2D
 
 
 func _ready():
-	get_tree().get_root().get_node("BaseLevel/CanvasLayer/Label").number_of_bananas += 1
+	get_tree().get_root().get_node("JuulesBananas/CanvasLayer/Label").number_of_bananas += 1
 
 func _on_body_entered(body):
 	if body.name == "Player" and self.visible:
 		self.visible = false
 		print("banana?!")
-		get_tree().get_root().get_node("BaseLevel/CanvasLayer/Label").count += 1
+		get_tree().get_root().get_node("JuulesBananas/CanvasLayer/Label").count += 1
