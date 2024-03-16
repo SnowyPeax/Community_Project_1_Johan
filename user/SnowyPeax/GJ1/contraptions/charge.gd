e2c7313e327cbe96feee3eeef59d2dead12e3b36
SnowyPeax <johbr@itu.dk>
2024-03-16 16:52:08 +0100

    merge

--- user/SnowyPeax/GJ1/contraptions/charge.gd
+++ user/SnowyPeax/GJ1/contraptions/charge.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var speed = 30
 @onready var player = Levelmanager.get_player()
 
+func _ready():
+	$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,3 +18,7 @@ func _on_body_entered(body):
 	if body == player:
 		player.die()
 		
+
+
+func _on_timer_timeout():
+	queue_free()
